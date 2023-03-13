2214f5c634aeef952d9f48653a8a0b77a25649db
Mark Snyder <Snyder4000@yahoo.com>
2023-03-13 14:07:56 +0000

    Completed Lab CharComparison

--- src/main/java/CharComparison.java
+++ src/main/java/CharComparison.java
@@ -11,6 +11,28 @@ public class CharComparison {
      * @return -1 if A is less than B, 1 if A is greater than B, and 0 if the two arrays are identical.
      */
     public int compare(char[] a, char[] b){
-        return 0;
+        int answer = 0;
+        if(a.length > b.length){
+            answer = 1;
+        }
+        else if(a.length == b.length){
+            for(int i = 0; i < a.length; i++){
+                if(a[i] > b[i]){
+                    answer =  1;
+                    break;
+                }
+                else if(a[i] < b[i]){
+                    answer = -1;
+                    break;
+                }
+                else{
+                    answer = 0;
+                }
+            }
+        }
+        else{
+            answer = -1;
+        }
+        return answer;
     }
 }
